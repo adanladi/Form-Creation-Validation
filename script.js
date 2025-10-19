10ed0afd39573da0fc017c89ad91ae9e0a2b464f
Amos Danladi <amosdanladi98@gmail.com>
2025-10-19 19:34:06 +0100

    Add registration form validation in script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById("form-feedback");
+
+  form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    let isValid = true;
+
+    let messages = [];
+
+    //username
+
+    if (username.length < 3) {
+      isValid = false;
+      messages.push("error message user name");
+    }
+
+    //email
+
+    if (!email.includes("@") || !email.includes(".")) {
+      isValid = false;
+      messages.push("error message email");
+    } 
+
+    //password
+
+    if (password.length < 8) {
+      isValid = false;
+      messages.push("error message password");
+    }
+
+    feedbackDiv.style.display = "block";
+    if (isValid) {
+      feedbackDiv.textContent = "Registration successful!";
+      feedbackDiv.style.color = "#28a745";
+    } else {
+      feedbackDiv.innerHTML = messages.join('<br>');
+      feedbackDiv.style.color = "#dc3545";
+      console.log(messages.join('<br>'))
+    }
+
+  });
+
+});
